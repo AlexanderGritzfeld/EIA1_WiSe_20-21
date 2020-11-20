41d08d1e638f24425880b529a6add45e107eb8f4
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2020-11-20 16:52:47 +0100

    Update script.ts

--- Aufgabe_05/script.ts
+++ Aufgabe_05/script.ts
@@ -9,3 +9,16 @@ var europe18 : number = 4209.3;
 
 var northamerica08 : number = 6600.4;
 var northamerica18 : number = 6035.6;
+
+var asia08 : number = 12954.7;
+var asia18 : number = 16274.1;
+
+var australia08 : number = 1993;
+var australia18 : number = 2100.5;
+
+var gesamt08 : number = africa08 + southamerica08 + europe08 + northamerica08 + asia08 +australia08;
+var gesamt18 : number = africa18 + southamerica18 + europe18 + northamerica18 + asia18 +australia18;
+
+
+console.log("Die Emission von Afrika ist: " + africa18 + " kg CO2")
+console.log("Relativ zur Gesamtemission der Welt verursacht Afrika damit " + Math.round(africa18/gesamt18*100) + "%")
