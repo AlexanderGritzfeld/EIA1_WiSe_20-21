b137b196a429d535757d978e88e6ac044342bcef
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2020-11-20 12:28:47 +0100

    Create script.ts
    das type script

---
+++ Aufgabe_05/script.ts
@@ -0,0 +1,11 @@
+var africa08 : number = 1028;
+var africa18 : number = 1235.5;
+
+var southamerica08 : number = 1132.6;
+var southamerica18 : number = 1261.5;
+
+var europe08 : number = 4965.7;
+var europe18 : number = 4209.3;
+
+var northamerica08 : number = 6600.4;
+var northamerica18 : number = 6035.6;
