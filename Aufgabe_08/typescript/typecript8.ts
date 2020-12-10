970ce8c98593bd56c12d0400b5d9f2f41a54e9fc
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2020-12-10 22:42:35 +0100

    Update typecript8.ts
    Ich hab' kein Bock mehr. Es will nicht machen was ich ihm sage, obwohl ichdoch alles wie im Video gemacht habe/ beschrieben wurde...Es ist spät ich gehe Schlafen

--- Aufgabe_08/typescript/typecript8.ts
+++ Aufgabe_08/typescript/typecript8.ts
@@ -13,31 +13,31 @@ drumPad[7] = new Audio("./assets/laugh-2.mp3");
 drumPad[8] = new Audio("./assets/snare.mp3");
 
 //Eventlistener (erstmal nur vom Drum Pad)
-document.querySelector("#pad1").addEventListener("click", function() {
+document.querySelector("#pad1").addEventListener("click", function(): void {
     playAudio(0);
 });
-document.querySelector("#pad2").addEventListener("click", function() {
+document.querySelector("#pad2").addEventListener("click", function(): void {
     playAudio(1);
 });
-document.querySelector("#pad3").addEventListener("click", function() {
+document.querySelector("#pad3").addEventListener("click", function(): void {
     playAudio(2);
 });
-document.querySelector("#pad4").addEventListener("click", function() {
+document.querySelector("#pad4").addEventListener("click", function(): void {
     playAudio(3);
 });
-document.querySelector("#pad5").addEventListener("click", function() {
+document.querySelector("#pad5").addEventListener("click", function(): void {
     playAudio(4);
 });
-document.querySelector("#pad6").addEventListener("click", function() {
+document.querySelector("#pad6").addEventListener("click", function(): void {
     playAudio(5);
 });
-document.querySelector("#pad7").addEventListener("click", function() {
+document.querySelector("#pad7").addEventListener("click", function(): void {
     playAudio(6);
 });
-document.querySelector("#pad8").addEventListener("click", function() {
+document.querySelector("#pad8").addEventListener("click", function(): void {
     playAudio(7);
 });
-document.querySelector("#pad9").addEventListener("click", function() {
+document.querySelector("#pad9").addEventListener("click", function(): void {
     playAudio(8);
 });
 
@@ -47,25 +47,40 @@ function playAudio(i: number): void {
 };
 
 //jetzt kommen wir zum Teil B
-document.querySelector("#playbutton").addEventListener("click", function() {
+document.querySelector("#playbutton").addEventListener("click", function(): void {
     setInterval(function() {
         playKnopf();
     }, 500);
     });
 
 //die Funktion für den PlayButton
-function playKnopf() {
+function playKnopf(): void {
     drumPad[5].play();
     drumPad[8].play();
     drumPad[4].play();
-};    
+}    
+
+//jetzt soll, wenn man den Play Button drückt dieser verschwinden und der Stop-Button auftauchen (und umgekehrt)
+
+/*zunächst einmal definiere ich dat, damit ich es später einfacher habe. Ich kann mir dann nämlich
+"document.get..." zu scripten sparen */
+const myPlayBtn: HTMLElement = document.getElementById("play");
+const myStopBtn: HTMLElement = document.getElementById("stop");
+
+/*Event bei welcher meiner Funktion (die später kommt) sagt, welche Werte sie nehmen soll beim Klick auf dem speziellen Element */
+myPlayBtn.addEventListener("click", function (): void {
+    toggleClasses(this, myStopBtn); 
+});
+//nochmal dasselbe nur umgekehrt
+myStopBtn.addEventListener("click", function (): void {
+    toggleClasses(this, myPlayBtn); 
+});
+
+//die eigentliche Funktion, die dahinter steckt
+function toggleClasses( firstHTMLElement: HTMLElement, secondHTMLElement: HTMLElement): void { //mit Platzhaltern, die von den Eventlistener befüllt werden
+    firstHTMLElement.classList.add("is-hidden"); //fügt definierten ersten Element Klasse hinzu --> verschwindet
+    secondHTMLElement.classList.remove("is-hidden"); //entfernt von zweitem Element Klasse --> ploppt auf
+    console.log("Hallo?");
+}
 
 //Schalter für Record-Button
-var btn: HTMLElement = document.querySelector("#myBtn");
-btn.addEventListener("click", function()
-if (btn.getAttribute("class") == "active" ) {
-btn.setAttribute("class", "");
-}
-else{
-    btn.setAttribute("class", "active");
-}
