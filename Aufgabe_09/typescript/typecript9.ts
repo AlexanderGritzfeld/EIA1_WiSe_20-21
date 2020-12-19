af7929d912decc14726da8acf25fc829df2bdcd7
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2020-12-19 16:22:18 +0100

    Update typecript9.ts

--- Aufgabe_09/typescript/typecript9.ts
+++ Aufgabe_09/typescript/typecript9.ts
@@ -1,4 +1,24 @@
 // Eine zusätzliche Aufgabe, in der wir eine Anwendung programmieren sollen, welche uns helfen soll nicht mit unseren Aufgabe überfordert zu sein
 namespace L09 { //damit es nicht rummeckert, dass ich Namen schon vergeben habe
 
+    //Index
+    let index: number = 0;
+
+    //Interface für Objekte der Aufgabe(n)
+    interface Aufgabe {
+    content: string; //Was drinne steht
+    status: boolean; //für dat Häkchen
+}
+//und jetzt lasst uns doch spaßeshalber ein paar Aufgaben erstellen7
+    let dieAufgaben: Aufgabe[]  = [
+{
+    content: "Schöne Feiertage haben.",
+    status: false
+},
+{
+    content: "Dieser Aufgabe als gut empfinden. :)",
+    status: true
+}
+];
+
 }
