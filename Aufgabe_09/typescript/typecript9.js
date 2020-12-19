da99140e1e5a0515b9db9592993cb4ff8d0e702c
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2020-12-19 16:22:11 +0100

    Update typecript9.js

--- Aufgabe_09/typescript/typecript9.js
+++ Aufgabe_09/typescript/typecript9.js
@@ -1,1 +1,18 @@
+// Eine zusätzliche Aufgabe, in der wir eine Anwendung programmieren sollen, welche uns helfen soll nicht mit unseren Aufgabe überfordert zu sein
+var L09;
+(function (L09) {
+    //Index
+    var index = 0;
+    //und jetzt lasst uns doch spaßeshalber ein paar Aufgaben erstellen7
+    var dieAufgaben = [
+        {
+            content: "Schöne Feiertage haben.",
+            status: false
+        },
+        {
+            content: "Dieser Aufgabe als gut empfinden. :)",
+            status: true
+        }
+    ];
+})(L09 || (L09 = {}));
 //# sourceMappingURL=typecript9.js.map
