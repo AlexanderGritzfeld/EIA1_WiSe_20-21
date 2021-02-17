b0e3eeefdc9b538ca7f26f815086fe6ce787bfc3
AlexanderGritzfeld <72884293+AlexanderGritzfeld@users.noreply.github.com>
2021-02-17 13:46:47 +0100

    Update fin_script.ts

--- Aufgabe_5/typescript/fin_script.ts
+++ Aufgabe_5/typescript/fin_script.ts
@@ -1,7 +1,7 @@
 
 namespace fin { //damit es nicht rummeckert, dass ich Namen schon vergeben habe
 
-let cCheck: number;    
+let cCheck: string;    
 
 //Definition/ einfachere Deklaration von HTML-Elementen
 
@@ -22,8 +22,26 @@ let set2: HTMLElement;
 let is12: HTMLElement;
 let is16: HTMLElement;
 
-/*Karten an sich
-let karte: HTMLElement; */
+//Karten an sich
+
+/*Interface
+interface memoryL {
+    content: HTMLElement;
+} */
+
+let k1: HTMLElement;
+let k2: HTMLElement;
+let k3: HTMLElement;
+let k4: HTMLElement;
+let k5: HTMLElement;
+let k6: HTMLElement;
+let k7: HTMLElement;
+let k8: HTMLElement;
+let memoryL: HTMLElement[] = [k1, k2, k3, k4, k5, k6, k7, k8];
+
+
+
+
 
 challenge = document.querySelector(".challenge");
 
@@ -37,7 +55,10 @@ set2 = document.querySelector(".set2");
 is12 = document.querySelector(".is12");
 is16 = document.querySelector(".is16");
 
-//karte = document.querySelector(".karte");
+
+k1 = document.querySelector(".k1");
+k2 = document.querySelector(".k2");
+
 
 //Ende Definition
 
@@ -45,7 +66,7 @@ is16 = document.querySelector(".is16");
 mittel.addEventListener("click", function(): void {
     set2.classList.add("is-hidden");
 
-    cCheck = 2;
+    cCheck = "m";
 }); //Ende Mittel
 
 
@@ -55,33 +76,60 @@ leicht.addEventListener("click", function(): void {
     is12.classList.add("is-hidden");
     is16.classList.add("is-hidden");
 
-    cCheck = 1;
+    cCheck = "l";
 }); //Ende Leicht
 
 schwierig.addEventListener("click", function(): void {
-    cCheck = 3;
+    cCheck = "s";
 }); //Ende Schwierig
 
 //Pop-Up soll verschwinden nach dem Drücken
 cBttn.addEventListener("click", function(): void {
     challenge.classList.add("is-hidden");
+    verarbeite(); //das stößt den Verarbeitungsprozess an
 
 }); //Ende CBttn
 
 //jetzt sollen die Karten-Klassen verteilen werden; basierend auf Schwierigkeit
-if (cCheck == 1) {
-//Verteilung wie?
-}
 
-if (cCheck == 2) {
-    //Verteiliung mittel
-}
+function verarbeite(): void { //function, weil erst checken soll, wenn Wert definiert wird
+switch (cCheck) {
 
-if (cCheck == 3) {
-    //Verteilung schwierig
-}
+    case "l":
+        //console.log("leicht");
+        k2.classList.add("rot");
+        k1.classList.add("zebra");
+        break;
+    case "m":
+        //console.log("mittel");
+        break;    
+}//Ende switch case
+}//Ende function verarbeite
 
+/*leicht
+if (cCheck == "l") {
+    console.log("Eins");
+} else {
 
+    //mittel
+    if (cCheck == "m") {
+        k1.classList.add("red");
+        k2.classList.add("zebra");
+        console.log("Hallo");
+    } else {
+
+        //schwierig
+        if (cCheck == "s") {
+            console.log("Drei");
+        }
+    }// Ende elseM
+}//Ende elseL
+*/
+
+//Test
+document.querySelector(".chatchat").addEventListener("click", function(): void {
+console.log(cCheck);
+});
 
 
 
